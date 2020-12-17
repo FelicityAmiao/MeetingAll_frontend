1f30f17ddb2a31f9d27db91f6f78ed315fb62583
VincentHuang <vincent.huang@oocl.com>
2020-12-18 00:14:04 +0800

    add recorder page

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-const loginPage = () => import('../views/Login')
+const loginPage = () => import('../views/login')
+const recorderPage = () => import('../views/recorder')
 
 const routes = [
   {
     path: '/',
+    component: recorderPage
+  },
+  {
+    path: '/recorder',
     component: loginPage
   }
 ]
