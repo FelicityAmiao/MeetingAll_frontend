43ca4514285acb1600412234ad095f1cf715f57c
LIANGAL2 <alvar.liang@cargosmart.com>
2020-12-28 09:48:22 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-tabs': 'off'
+    'no-tabs': 'off',
+    'semi': ['error', 'always'],
+    'vue/html-quotes': ['error', 'single'],
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
