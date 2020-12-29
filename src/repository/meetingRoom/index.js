0d364a99921fb6578da4d3d5ed3cf27c002b5a42
LIANGAL2 <alvar.liang@cargosmart.com>
2020-12-29 13:20:47 +0800

    update meeting room device control function

--- src/repository/meetingRoom/index.js
+++ src/repository/meetingRoom/index.js
@@ -1,7 +1,13 @@
-import { get } from '../../utils/http';
+import { get, post } from '../../utils/http';
 
 export const queryAllMeetingRooms = async () => {
   let url = `/meetingRooms`;
   const body = await get(url);
+  return body.data;
+};
+
+export const updateDeviceStatus = async content => {
+  let url = `/meetingRooms`;
+  const body = await post(url, content);
   return body.data;
 };
