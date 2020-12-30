f1015856b2725326eec9e6122654b9a0a958ee73
pengsm <smith.peng@cargosmart.com>
2020-12-30 14:39:11 +0800

    [Smith] fix: move getter to user instead of a getter file

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,10 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import createLogger from 'vuex/dist/logger';
 import modules from './modules';
-import getters from './getter';
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules,
-  getters,
   plugins: [
     ...process.env.NODE_ENV === 'development' ? [ createLogger() ] : []
   ],
