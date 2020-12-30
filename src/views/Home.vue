891c403a65bee59a0435719162274de29f743c50
linsa4 <samson.lin@oocl.com>
2020-12-30 17:19:46 +0800

    add logout function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,15 @@
           </el-col>
         </el-row>
         <div class='right-top-menu'>
-          <p>user</p>
+          <el-dropdown @command='handleCommand'>
+            <span class='el-dropdown-link'>
+              {{username}}<i class='el-icon-arrow-down el-icon--right'></i>
+            </span>
+            <el-dropdown-menu slot='dropdown'>
+              <el-dropdown-item command='changePassword'>修改密码</el-dropdown-item>
+              <el-dropdown-item command='logout'>退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
         </div>
       </el-header>
       <el-main id='mainContainer' style='padding: 0'>
@@ -26,19 +34,59 @@
         </keep-alive>
       </el-main>
     </el-container>
+    <el-dialog title='修改密码' :visible.sync='changePasswordFormVisible'>
+      <el-form :model='changePasswordForm' :rules='changePasswordRules' status-icon>
+        <el-form-item label='旧密码' :label-width='changePasswordFormLabelWidth' prop='originPassword'>
+          <el-input type='password' v-model='changePasswordForm.originPassword' clearable></el-input>
+        </el-form-item>
+        <el-form-item label='新密码' :label-width='changePasswordFormLabelWidth' prop='newPassword'>
+          <el-input type='password' v-model='changePasswordForm.newPassword' clearable></el-input>
+        </el-form-item>
+        <el-form-item label='确认密码' :label-width='changePasswordFormLabelWidth' prop='confirmPassword'>
+          <el-input type='password' v-model='changePasswordForm.confirmPassword' clearable></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot='footer' class='dialog-footer'>
+        <el-button @click='changePasswordFormVisible = false'>取 消</el-button>
+        <el-button type='primary' @click='changePasswordFormVisible = false'>确 定</el-button>
+      </div>
+    </el-dialog>
   </el-container>
 </template>
 
 <script>
 import Nav from '../components/Nav.vue';
+import { validatePwd } from '../utils/validate';
 
 export default {
   name: 'Home',
   components: { Nav },
   data () {
+    let validatePwd2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'));
+      } else if (value !== this.changePasswordForm.newPassword) {
+        callback(new Error('两次输入密码不一致'));
+      } else {
+        callback();
+      }
+    };
     return {
       isCollapse: false,
-      height: ''
+      height: '',
+      username: this.$store.getters.username,
+      changePasswordFormVisible: false,
+      changePasswordFormLabelWidth: '100px',
+      changePasswordForm: {
+        originPassword: '',
+        newPassword: '',
+        confirmPassword: ''
+      },
+      changePasswordRules: {
+        originPassword: [{ validator: validatePwd, trigger: 'blur' }],
+        newPassword: [{ validator: validatePwd, trigger: 'blur' }],
+        confirmPassword: [{ validator: validatePwd2, trigger: 'blur' }]
+      }
     };
   },
   mounted () {
@@ -51,6 +99,21 @@ export default {
   methods: {
     connectWebSocket () {
       this.$store.dispatch('socket/connect');
+    },
+    handleCommand (command) {
+      if (command === 'changePassword') {
+        this.changePasswordFormVisible = true;
+      } else {
+        this.$confirm('您是否要退出?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$store.dispatch('user/logout').then(() => {
+            this.$router.push('/login');
+          });
+        });
+      }
     }
   }
 };
@@ -82,5 +145,13 @@ export default {
   display: flex;
   height: 60px;
   vertical-align: middle;
+  margin-top: 20px;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409EFF;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
 }
 </style>
