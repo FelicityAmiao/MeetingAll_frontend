8cd7886617525b5eccf178a1c1634ddafc2145f2
AKIN GAN <akin.j.a.gan@oocl.com>
2020-12-31 14:01:17 +0800

    add home page

---
+++ src/views/HomePage.vue
@@ -0,0 +1,231 @@
+<template>
+  <div>
+    <div class='header'>
+      <el-col :span='8'>
+        <div class='system-icon'>Meeting All</div>
+      </el-col>
+      <el-col :span='16'>
+        <div class='right-top-menu' v-if='username !== "" && username != null'>
+        <el-dropdown @command='handleCommand'>
+          <span class='el-dropdown-link'>
+            <i class='el-icon-s-custom'></i> {{username}}<i class='el-icon-arrow-down el-icon--right'></i>
+          </span>
+          <el-dropdown-menu slot='dropdown'>
+            <el-dropdown-item command='changePassword'>修改密码</el-dropdown-item>
+            <el-dropdown-item command='logout'>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        </div>
+      </el-col>
+    </div>
+      <div class='meeting-room-panel'>
+        <el-carousel :interval='4000' type='card' height='360px'>
+          <el-carousel-item v-for='(item, index) in officeList' :key='index'>
+            <div @click='selectMeetingRoom(item)'>
+              <h3 class='office-label'>{{item}}</h3>
+              <img class='meeting-room-img' src='https://wpimg.wallstcn.com/e7d23d71-cf19-4b90-a1cc-f56af8c0903d.png'>
+            </div>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+      <div class='menu-btn-group'>
+        <el-col :span='8'>
+          <el-dropdown>
+            <el-button class='menu-btn' icon='el-icon-location' type='primary'>
+              会议室
+              <i class='el-icon-arrow-down el-icon--right'></i>
+            </el-button>
+            <el-dropdown-menu slot='dropdown' v-for='(item, index) in officeList' :key='index'>
+              <el-dropdown-item @click.native='selectMeetingRoom(item)'>{{item}}</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+        <el-col :span='8'>
+          <el-button class='menu-btn' icon='el-icon-service' type='primary'>开会</el-button>
+        </el-col>
+        <el-col :span='8'>
+          <el-button class='menu-btn' icon='el-icon-document' type='primary'>会议记录</el-button>
+        </el-col>
+      </div>
+    </div>
+</template>
+
+<script>
+import { validatePwd } from '../utils/validate';
+import md5 from 'js-md5';
+import { post } from '../utils/http';
+import { queryMeetingRoomList } from '../service/meetingRoom/index';
+import _ from 'lodash';
+
+export default {
+  name: 'HomePage',
+  data () {
+    let validatePwd2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'));
+      } else if (value !== this.changePasswordForm.newPassword) {
+        callback(new Error('两次输入密码不一致'));
+      } else {
+        callback();
+      }
+    };
+    return {
+      isCollapse: false,
+      showClose: false,
+      username: sessionStorage.getItem('username'),
+      changePasswordFormVisible: false,
+      changePasswordFormLabelWidth: '100px',
+      changePasswordForm: {
+        originPassword: '',
+        newPassword: '',
+        confirmPassword: ''
+      },
+      changePasswordRules: {
+        originPassword: [{ validator: validatePwd, trigger: 'blur' }],
+        newPassword: [{ validator: validatePwd, trigger: 'blur' }],
+        confirmPassword: [{ validator: validatePwd2, trigger: 'blur' }]
+      },
+      officeList: [],
+      meetingRoomGroup: {}
+    };
+  },
+  created () {
+    this.queryMeetingRoomList();
+  },
+  mounted () {
+    // window.addEventListener('resize', () => {
+    //   this.height = window.innerHeight;
+    // });
+    this.connectWebSocket();
+  },
+  methods: {
+    changePassword () {
+      this.$refs['changePasswordForm'].validate((valid) => {
+        if (valid) {
+          let originPwd = md5(this.changePasswordForm.originPassword);
+          let newPwd = md5(this.changePasswordForm.newPassword);
+          let url = 'user/password';
+          post(url, {
+            username: this.username,
+            originPassword: originPwd,
+            newPassword: newPwd
+          }).then((res) => {
+            if (res.data.success) {
+              this.$message.success('密码重设成功，请重新登录！');
+              this.$store.dispatch('user/logout').then(() => {
+                this.$router.push('/login');
+              });
+            } else {
+              this.$message.error(res.data.msg);
+            }
+          }).catch(() => {
+            this.$message.error('密码重设失败，请重试！');
+          });
+        } else {
+          return false;
+        }
+      });
+    },
+    connectWebSocket () {
+      this.$store.dispatch('socket/connect');
+    },
+    handleCommand (command) {
+      if (command === 'changePassword') {
+        this.changePasswordFormVisible = true;
+      } else {
+        this.$confirm('您是否要退出?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$store.dispatch('user/logout').then(() => {
+            this.$router.push('/login');
+          });
+        });
+      }
+    },
+    async queryMeetingRoomList () {
+      let result = await queryMeetingRoomList();
+      if (result) {
+        this.meetingRoomGroup = _.groupBy(result, 'office');
+        this.officeList = _.keys(this.meetingRoomGroup);
+      }
+    },
+    selectMeetingRoom (office) {
+      const meetingRoomDetail = this.meetingRoomGroup[office];
+      this.$router.push({
+        path: '/meetingRoomDetail',
+        query: {
+          meetingRoomDetail
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .header {
+    width: 100%;
+    height: 65px;
+    color: white;
+    box-shadow: rgba(0, 21, 41, 0.08) 0 1px 4px;
+    overflow: hidden;
+    background: rgba(116, 137, 158, 0.3);
+    display: flex;
+  }
+
+  .meeting-room-panel {
+    text-align: center;
+    padding: 100px;
+  }
+
+  .menu-btn-group {
+    text-align: center;
+    padding: 10px 36%;
+    /*width: 30%;*/
+  }
+
+  .menu-btn {
+    background: rgba(116, 137, 158, 0.3);
+    border: rgba(116, 137, 158, 0.3);
+    width: 150px;
+  }
+
+  .office-label {
+    position: fixed;
+    padding: 10px;
+    color: white;
+    font-size: 18px;
+  }
+
+  .el-dropdown-link {
+    cursor: pointer;
+    color: white;
+  }
+
+  .meeting-room-img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .right-top-menu {
+    position: absolute;
+    right: 20px;
+    height: 60px;
+    vertical-align: middle;
+    margin-top: 20px;
+    color: white;
+  }
+
+  .system-icon {
+    position: absolute;
+    left: 20px;
+    height: 60px;
+    vertical-align: middle;
+    margin-top: 20px;
+    font-size: 18px;
+    font-style: revert;
+  }
+
+</style>
