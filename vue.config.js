361c2936b0e33ded263691f3a12f0341f0b48e75
CHENFE2 <felicity.chen@cargosmart.com>
2020-12-31 14:22:48 +0800

    [Felicity] node build performance tunning

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,30 @@
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
-const defaultPlugins = [
-  new CompressionWebpackPlugin({
-    test: /\.js$|\.html$|\.css$/,
+const compress = new CompressionWebpackPlugin(
+  {
+    filename: info => {
+      return `${info.path}.gz${info.query}`;
+    },
+    algorithm: 'gzip',
     threshold: 10240,
-    minRatio: 0.8
-  })
+    test: new RegExp(
+      '\\.(' +
+      ['js'].join('|') +
+      ')$'
+    ),
+    minRatio: 0.8,
+    deleteOriginalAssets: false
+  }
+);
+
+const defaultPlugins = [
+  // change to gzip, but seems same
+  // new CompressionWebpackPlugin({
+  //   test: /\.js$|\.html$|\.css$/,
+  //   threshold: 10240,
+  //   minRatio: 0.8
+  // }),
+  compress
 ];
 
 module.exports = {
@@ -13,6 +32,13 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
   runtimeCompiler: true,
   devServer: {
+    before (app, server) {
+      app.get(/.*.(js)$/, (req, res, next) => {
+        req.url = req.url + '.gz';
+        res.set('Content-Encoding', 'gzip');
+        next();
+      });
+    },
     disableHostCheck: process.env.NODE_ENV !== 'production',
     proxy: {
       '/api': {
@@ -31,12 +57,19 @@ module.exports = {
       config.optimization.splitChunks({	// 将大文件的代码分割成两个文件，如app.js(9.5M)分割成app.js(2.5M)+venders~app.js(7M)
         chunks: 'all'
       });
+      config.externals({
+        'vue': 'Vue',
+        'vuex': 'Vuex',
+        'vue-router': 'VueRouter',
+        'mint-ui': 'MINT', // 需用MINT
+        'axios': 'axios'
+      });
     }
   },
   configureWebpack: config => {	// 不管是否打包都可以压缩压缩
     defaultPlugins.forEach(plugin => config.plugins.push(plugin));
   },
-  watchOptions: {
-    ignored: /node_modules/
+  css: { // 提取css from app.vender.js
+    extract: true
   }
 };
